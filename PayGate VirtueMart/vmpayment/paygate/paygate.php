c0eaac0f61dd00349c592660c26e686f4a9b939f
Bruce Atkinson <bruce@appinlet.com>
2020-07-28 07:23:47 +0200

    2020-07-28: v1.0.3
    Update for Virtuemart 3.8.4.
    Code quality improve.
    Add iFrame functionality.

--- PayGate VirtueMart/vmpayment/paygate/paygate.php
+++ PayGate VirtueMart/vmpayment/paygate/paygate.php
@@ -1,12 +1,12 @@
 <?php
 /*
- * Copyright (c) 2018 PayGate (Pty) Ltd
+ * Copyright (c) 2020 PayGate (Pty) Ltd
  *
  * Author: App Inlet (Pty) Ltd
- * 
+ *
  * Released under the GNU General Public License
  */
-defined( '_JEXEC' ) or die();
+defined( '_JEXEC' ) || die();
 
 # Get plugin class
 if ( !class_exists( 'vmPSPlugin' ) ) {
@@ -17,7 +17,7 @@ if ( !class_exists( 'vmPSPlugin' ) ) {
  * Class plgVmPaymentPayGate
  *
  * @author  App Inlet (Pty) Ltd
- * @version 1.0.2
+ * @version 1.0.3
  * @link    https://developer.paygate.co.za/products/shoppingcarts
  */
 class plgVmPaymentPayGate extends vmPSPlugin
@@ -27,6 +27,8 @@ class plgVmPaymentPayGate extends vmPSPlugin
 
     public $paygateId;
     public $paygateKey;
+
+    const ORDER_NUMBER = 'Order #';
 
     /**
      * Save settings
@@ -51,6 +53,7 @@ class plgVmPaymentPayGate extends vmPSPlugin
             'test'          => array( 0, 'int' ),
             'id'            => array( '', 'char' ),
             'key'           => array( '', 'char' ),
+            'pw3iframe'     => array( '', 'char' ),
             'successful'    => array( '', 'char' ),
             'failed'        => array( '', 'char' ),
             'payment_logos' => array( '', 'char' ),
@@ -64,11 +67,7 @@ class plgVmPaymentPayGate extends vmPSPlugin
      */
     public function _is_curl_installed()
     {
-        if ( in_array( 'curl', get_loaded_extensions() ) ) {
-            return true;
-        } else {
-            return false;
-        }
+        return in_array( 'curl', get_loaded_extensions() );
     }
 
     /**
@@ -78,7 +77,7 @@ class plgVmPaymentPayGate extends vmPSPlugin
     public function generateChecksum( $postData, $paygetkey )
     {
         $checksum = '';
-        foreach ( $postData as $key => $value ) {
+        foreach ( $postData as $value ) {
             if ( $value != '' ) {
                 $checksum .= $value;
             }
@@ -104,13 +103,12 @@ class plgVmPaymentPayGate extends vmPSPlugin
      */
     public function getTableSQLFields()
     {
-        $fields = array(
+        return array(
             'id'                  => 'INT(11) NOT NULL AUTO_INCREMENT',
             'virtuemart_order_id' => 'INT(11)',
             'paygate_post'        => 'varchar(2500)',
             'payment_name'        => 'VARCHAR(75) NOT NULL DEFAULT "PayGate"',
         );
-        return $fields;
     }
 
     /**
@@ -129,6 +127,8 @@ class plgVmPaymentPayGate extends vmPSPlugin
             return false;
         }
 
+        $iframe = $method->get( 'pw3iframe' );
+
         if ( !$this->selectedThisElement( $method->payment_element ) ) {
             return false;
         }
@@ -139,15 +139,35 @@ class plgVmPaymentPayGate extends vmPSPlugin
 
         $checksum   = $this->generateChecksum( $responseFields, $this->paygateKey );
         $processurl = $this->_processurl;
-        $html       = <<<HTML
+
+        // Add CSS for the IFrame
+        vmJsApi::css( 'paygate', 'plugins/vmpayment/paygate/' );
+        if ( $iframe !== '0' ) {
+            $html = <<<HTML
 <p>Kindly wait while you're redirected to PayGate ...</p>
-<form action="{$processurl}" method="post" name="redirect">
-		<input name="PAY_REQUEST_ID" type="hidden" value="{$responseFields['PAY_REQUEST_ID']}" />
-		<input name="CHECKSUM" type="hidden" value="{$checksum}" />
-</form>
-<script type="text/javascript">document.forms['redirect'].submit();</script>
+<div id="payPopup">
+    <div id="payPopupContent">
+        <form action="{$processurl}" method="post" name="redirect" target="pw3Iframe">
+            <input name="PAY_REQUEST_ID" type="hidden" value="{$responseFields['PAY_REQUEST_ID']}" />
+            <input name="CHECKSUM" type="hidden" value="{$checksum}" />
+        </form>
+        <iframe name="pw3Iframe" id="payPopupFrame"></iframe>
+        <script type="text/javascript">document.forms['redirect'].submit();</script>
+    </div>
+</div>
 HTML;
-        JRequest::setVar( 'html', $html );
+        } else {
+
+            $html = <<<HTML
+<p>Kindly wait while you're redirected to PayGate ...</p>
+        <form action="{$processurl}" method="post" name="redirect" >
+            <input name="PAY_REQUEST_ID" type="hidden" value="{$responseFields['PAY_REQUEST_ID']}" />
+            <input name="CHECKSUM" type="hidden" value="{$checksum}" />
+        </form>
+        <script type="text/javascript">document.forms['redirect'].submit();</script>
+HTML;
+        }
+        ( new JInput() )->set( 'html', $html );
         return false;
     }
 
@@ -159,64 +179,108 @@ HTML;
      */
     public function plgVmOnPaymentResponseReceived( &$html )
     {
-        $method = $_GET['pm'];
+        $method             = filter_var( $_GET['pm'], FILTER_SANITIZE_STRING );
+        $order              = filter_var( $_GET['on'], FILTER_SANITIZE_STRING );
+        $pm                 = filter_var( $_GET['pm'], FILTER_SANITIZE_STRING );
+        $itemid             = filter_var( $_GET['Itemid'], FILTER_SANITIZE_STRING );
+        $PAY_REQUEST_ID     = filter_var( $_POST['PAY_REQUEST_ID'], FILTER_SANITIZE_STRING );
+        $TRANSACTION_STATUS = filter_var( $_POST['TRANSACTION_STATUS'], FILTER_SANITIZE_STRING );
+        $CHECKSUM           = filter_var( $_POST['CHECKSUM'], FILTER_SANITIZE_STRING );
+        if ( !$order ) {
+            return false;
+        }
+
+        if ( isset( $_GET['ifr'] ) ) {
+            $ifr = filter_var( $_GET['ifr'], FILTER_SANITIZE_STRING );
+        }
+
         $method = $this->getVmPluginMethod( $method );
         if ( !$method ) {
             return false;
+        }
+
+        // Check if iFrame is enabled
+        $iframe = $method->get( 'pw3iframe' );
+        if ( $iframe && $ifr !== 'true' ) {
+            $root = JROUTE::_( JURI::root() );
+            $url  = <<<URL
+{$root}index.php?option=com_virtuemart&view=pluginresponse&task=pluginresponsereceived&on={$order}&pm={$pm}&Itemid={$itemid}&ifr=true&PAY_REQUEST_ID={$PAY_REQUEST_ID}&TRANSACTION_STATUS={$TRANSACTION_STATUS}&CHECKSUM={$CHECKSUM}
+URL;
+            echo '<script>window.top.location.href="' . $url . '";</script>';
+            exit;
+        } elseif ( $iframe && $ifr === 'true' ) {
+            $_POST                       = [];
+            $_POST['PAY_REQUEST_ID']     = filter_var( $_GET['PAY_REQUEST_ID'], FILTER_SANITIZE_STRING );
+            $_POST['TRANSACTION_STATUS'] = filter_var( $_GET['TRANSACTION_STATUS'], FILTER_SANITIZE_STRING );
+            $_POST['CHECKSUM']           = filter_var( $_GET['CHECKSUM'], FILTER_SANITIZE_STRING );
         }
 
         if ( !$this->selectedThisElement( $method->payment_element ) ) {
             return false;
         }
 
-        $order = $_GET['on'];
-        if ( !$order ) {
-            return false;
-        }
+        $status    = $_POST['TRANSACTION_STATUS'];
+        $model     = VmModel::getModel( 'orders' );
+        $id        = $model->getOrderIdByOrderNumber( $order );
+        $or        = $model->getOrder( $id );
+        $detail    = $or['details']['BT'];
+        $reference = self::ORDER_NUMBER . $detail->order_number;
 
-        $status = $_POST['TRANSACTION_STATUS'];
-        $model  = VmModel::getModel( 'orders' );
-        $id     = $model->getOrderIdByOrderNumber( $order );
-        if ( $model->getOrder( $id )['details']['BT']->order_status == 'P' ) {
-            if ( $status == '1' ) {
-                $displayComment                = vmText::_( 'VMPAYMENT_PAYGATE_SUCCESSFUL_PAYMENT' );
-                $details['customer_notified']  = 1;
-                $details['order_status']       = $method->successful;
-                $details['comments']           = vmText::sprintf( 'VMPAYMENT_PAYGATE_SUCCESSFUL_PAYMENT_COMMENT', $_POST['PAY_REQUEST_ID'] );
-                $fields['virtuemart_order_id'] = $id;
-            } else {
-                if ( $status == '2' ) {
-                    $details['comments'] = vmText::_( 'VMPAYMENT_PAYGATE_DECLINED_PAYMENT' );
-                    $displayComment      = vmText::_( 'VMPAYMENT_PAYGATE_DECLINED_PAYMENT_COMMENT' );
+        // Check validity of checksum before going any further
+        $sent_sum  = array_pop( $_POST );
+        $paygateid = $method->get( 'id' );
+        $paygatekey = $method->get( 'key' );
+        if ($method->get( 'test' )) {
+            $paygateid = '10011072130';
+            $paygatekey = 'secret';
+        }
+        $our_sum   = md5( $paygateid . implode( '', $_POST ) . $reference . $paygatekey );
+        $sumok     = hash_equals( $sent_sum, $our_sum );
+        if ( $sumok ) {
+            if ( $detail->order_status == 'P' ) {
+                if ( $status == '1' ) {
+                    $displayComment                = vmText::_( 'VMPAYMENT_PAYGATE_SUCCESSFUL_PAYMENT' );
+                    $details['customer_notified']  = 1;
+                    $details['order_status']       = $method->successful;
+                    $details['comments']           = vmText::sprintf( 'VMPAYMENT_PAYGATE_SUCCESSFUL_PAYMENT_COMMENT', $_POST['PAY_REQUEST_ID'] );
+                    $fields['virtuemart_order_id'] = $id;
                 } else {
-                    $details['comments'] = vmText::_( 'VMPAYMENT_PAYGATE_FAILURE_PAYMENT' );
-                    $displayComment      = vmText::_( 'VMPAYMENT_PAYGATE_FAILURE_PAYMENT_COMMENT' );
+                    if ( $status == '2' ) {
+                        $details['comments'] = vmText::_( 'VMPAYMENT_PAYGATE_DECLINED_PAYMENT' );
+                        $displayComment      = vmText::_( 'VMPAYMENT_PAYGATE_DECLINED_PAYMENT_COMMENT' );
+                    } else {
+                        $details['comments'] = vmText::_( 'VMPAYMENT_PAYGATE_FAILURE_PAYMENT' );
+                        $displayComment      = vmText::_( 'VMPAYMENT_PAYGATE_FAILURE_PAYMENT_COMMENT' );
+                    }
+                    $details['customer_notified']  = 1;
+                    $details['order_status']       = $method->failed;
+                    $fields['virtuemart_order_id'] = $id;
                 }
-                $details['customer_notified']  = 1;
-                $details['order_status']       = $method->failed;
-                $fields['virtuemart_order_id'] = $id;
+
+                $post = '';
+                foreach ( $_POST as $k => $v ) {
+                    $post .= $k . '="' . $v . '"|';
+                }
+
+                $fields['paygate_post'] = substr( $post, 0, -1 );
+                $fields['payment_name'] = 'PayGate';
+
+                $this->storePSPluginInternalData( $fields, 'virtuemart_order_id', true );
+                $model->updateStatusForOneOrder( $id, $details, true );
             }
 
-            $post = '';
-            foreach ( $_POST as $k => $v ) {
-                $post .= $k . '="' . $v . '"|';
+            if ( !class_exists( 'VirtueMartCart' ) ) {
+                require JPATH_VM_SITE . DS . 'helpers' . DS . 'cart.php';
             }
 
-            $fields['paygate_post'] = substr( $post, 0, -1 );
-            $fields['payment_name'] = 'PayGate';
-
-            $this->storePSPluginInternalData( $fields, 'virtuemart_order_id', true );
-            $model->updateStatusForOneOrder( $id, $details, true );
+            $cart = VirtueMartCart::getCart();
+            if ( $status == 1 ) {
+                $cart->emptyCart();
+            }
+        } else {
+            $displayComment = 'The transaction could not be verified. Please contact the store';
         }
 
-        if ( !class_exists( 'VirtueMartCart' ) ) {
-            require JPATH_VM_SITE . DS . 'helpers' . DS . 'cart.php';
-        }
-
-        $cart = VirtueMartCart::getCart();
-        if ( $status == 1 ) {
-            $cart->emptyCart();
-        }
         $html = <<<HTML
 <p>{$displayComment}</p>
 HTML;
@@ -239,7 +303,6 @@ HTML;
         $paygate_data = array();
 
         $notify_data = array();
-        $post_data   = '';
         // Get notify data
         if ( !$errors ) {
             $paygate_data = $this->getPostData();
@@ -279,7 +342,6 @@ HTML;
         if ( !$errors ) {
 
             foreach ( $paygate_data as $key => $val ) {
-                $post_data .= $key . '=' . $val . "\n";
                 $notify_data[$key] = stripslashes( $val );
 
                 if ( $key == 'PAYGATE_ID' ) {
@@ -289,7 +351,7 @@ HTML;
                     $checkSumParams .= $val;
                 }
 
-                if ( sizeof( $notify_data ) == 0 ) {
+                if ( empty( $notify_data ) ) {
                     $errors = true;
                 }
             }
@@ -300,7 +362,7 @@ HTML;
         if ( !$errors ) {
             $checkSumParams = md5( $checkSumParams );
             if ( $checkSumParams != $paygate_data['CHECKSUM'] ) {
-                !$errors = true;
+                $errors = true;
             }
         }
 
@@ -372,9 +434,9 @@ HTML;
         $address = (  ( $cart->ST == 0 ) ? $cart->BT : $cart->ST );
 
         $amount      = $cart_prices['salesPrice'];
-        $amount_cond = ( $amount >= $method->min_amount and $amount <= $method->max_amount
-            or
-            ( $method->min_amount <= $amount and ( $method->max_amount == 0 ) ) );
+        $amount_cond = ( $amount >= $method->min_amount && $amount <= $method->max_amount
+            ||
+            ( $method->min_amount <= $amount && ( $method->max_amount == 0 ) ) );
 
         $countries = array();
         if ( !empty( $method->countries ) ) {
@@ -393,11 +455,8 @@ HTML;
         if ( !isset( $address['virtuemart_country_id'] ) ) {
             $address['virtuemart_country_id'] = 0;
         }
-        if ( in_array( $address['virtuemart_country_id'], $countries ) || count( $countries ) == 0 ) {
-            if ( $amount_cond ) {
-                return true;
-            }
-
+        if (  ( in_array( $address['virtuemart_country_id'], $countries ) || empty( $countries ) ) && $amount_cond ) {
+            return true;
         }
 
         return false;
@@ -430,15 +489,42 @@ HTML;
                 $fields_string .= $key . '=' . urlencode( $value ) . '&';
             }
             $fields_string = rtrim( $fields_string, '&' );
-            $ch            = curl_init();
+            // Get host safely
+            $possibleHostSources   = array( 'HTTP_X_FORWARDED_HOST', 'HTTP_HOST', 'SERVER_NAME', 'SERVER_ADDR' );
+            $sourceTransformations = array(
+                "HTTP_X_FORWARDED_HOST" => function ( $value ) {
+                    $elements = explode( ',', $value );
+                    return trim( end( $elements ) );
+                },
+            );
+            $host = '';
+            foreach ( $possibleHostSources as $source ) {
+                if ( !empty( $host ) ) {
+                    break;
+                }
+
+                if ( empty( $_SERVER[$source] ) ) {
+                    continue;
+                }
+
+                $host = $_SERVER[$source];
+                if ( array_key_exists( $source, $sourceTransformations ) ) {
+                    $host = $sourceTransformations[$source]( $host );
+                }
+            }
+
+            // Remove port number from host
+            $host = trim( preg_replace( '/:\d+$/', '', $host ) );
+
+            $ch = curl_init();
             if ( !$this->isSsl() ) {
-                curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 0 );
-                curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 0 );
+                curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 1 );
+                curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 2 );
             }
             curl_setopt( $ch, CURLOPT_URL, $url );
             curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
             curl_setopt( $ch, CURLOPT_NOBODY, false );
-            curl_setopt( $ch, CURLOPT_REFERER, $_SERVER['HTTP_HOST'] );
+            curl_setopt( $ch, CURLOPT_REFERER, $host );
             curl_setopt( $ch, CURLOPT_POST, 1 );
             curl_setopt( $ch, CURLOPT_POSTFIELDS, $fields_string );
             $result = curl_exec( $ch );
@@ -453,11 +539,8 @@ HTML;
     public function isSsl()
     {
         $url = parse_url( JURI::base() );
-        if ( $url['scheme'] == 'https' ) {
-            return true;
-        } else {
-            return false;
-        }
+
+        return ($url['scheme'] == 'https');
     }
 
     public function getPostData()
@@ -471,7 +554,7 @@ HTML;
         }
 
         // Return "false" if no data was received
-        if ( sizeof( $nData ) == 0 ) {
+        if ( empty( $nData ) ) {
             return ( false );
         } else {
             return ( $nData );
@@ -531,7 +614,7 @@ HTML;
             return false;
         }
 
-        $this->logInfo( 'Order #' . $details->order_number . ' confirmed', 'message' );
+        $this->logInfo( self::ORDER_NUMBER . $details->order_number . ' confirmed', 'message' );
 
         if ( $method->test ) {
             $this->paygateId  = '10011072130';
@@ -558,7 +641,7 @@ HTML;
         $db->setQuery( $query );
         $currency_code3 = $db->loadResult();
 
-        $reference = 'Order #' . $details->order_number;
+        $reference = self::ORDER_NUMBER . $details->order_number;
         $amount    = number_format( $details->order_total * 100, 0, '', '' );
 
         $url        = JROUTE::_( JURI::root() . 'index.php?option=com_virtuemart&view=pluginresponse&task=pluginresponsereceived&on=' . $details->order_number . '&pm=' . $details->virtuemart_paymentmethod_id . '&Itemid=' . JRequest::getInt( 'Itemid' ) );
@@ -578,7 +661,7 @@ HTML;
             'EMAIL'            => $email,
             'NOTIFY_URL'       => $notify_url,
             'USER1'            => $details->order_number,
-            'USER3'            => 'virtuemart-v1.0.2',
+            'USER3'            => 'virtuemart-v1.0.3',
         );
 
         $initiateFields['CHECKSUM'] = $this->generateChecksum( $initiateFields, $this->paygateKey );
